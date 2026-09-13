8dc17376925b0bfccb8d2154395cf6939944a275
Kosmo零 <metafalica@gmx.com>
2026-09-13 23:28:08 +0400

    Попытка восстановить CarApp.java

--- src/main/java/ru/aston/CarApp.java
+++ src/main/java/ru/aston/CarApp.java
@@ -5,7 +5,6 @@ import ru.aston.UI.menu.MenuItem;
 import ru.aston.UI.state.ApplicationContext;
 
 public class CarApp {
-
     public static void main(String[] args) {
         try {
             System.out.println("=== CAR APPLICATION ===");
@@ -15,7 +14,8 @@ public class CarApp {
             MenuItem rootMenu = MenuFactory.createMainMenu();
 
             rootMenu.execute(context);
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             System.err.println("Critical error: " + e.getMessage());
             e.printStackTrace();
         }
